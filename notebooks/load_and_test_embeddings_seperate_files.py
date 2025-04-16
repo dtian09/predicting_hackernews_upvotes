0ec2b90c22d252a162dceeb30bbf05bc9f56fba6
Daniel Bryars <bryars@hotmail.com>
2025-04-16 17:41:27 +0000

    Updated to use seperate files

--- notebooks/load_and_test_embeddings_seperate_files.py
+++ notebooks/load_and_test_embeddings_seperate_files.py
@@ -5,23 +5,31 @@ import sys
 
 from scipy.spatial.distance import cosine
 
-
 #embeddings = torch.load("../data/word_embeddings_dim100.pt",weights_only=False)
 
 compare_word = "computer"
 if len(sys.argv) > 1:
     compare_word = sys.argv[1]
     
-filename = "word_embeddings_dim200_epoch10.pt"
-embeddings = torch.load(filename,weights_only=False)
+#filename = "word_embeddings_dim200_epoch10.pt"
+embeddings_filename = "../data/skip_gram_embeddings.pt"
+embeddings = torch.load(embeddings_filename,weights_only=False)
 
-print(f"compare_word: '{compare_word}' on '{filename}'")
+word_idx_filename = "../data/skip_gram_word_to_idx.pt"
+word_idx = torch.load(word_idx_filename,weights_only=False)
 
-compare_embedding = torch.tensor(embeddings[compare_word])
+idx_word_filename = "../data/skip_gram_idx_to_word.pt"
+idx_word = torch.load(idx_word_filename,weights_only=False)
+
+print(f"compare_word: '{compare_word}' on '{word_idx_filename}'")
+
+compare_idx = word_idx[compare_word]
+
+compare_embedding = torch.tensor(embeddings[compare_idx])
 
 # calculate the cosine similarity between the compare_embedding and all other embeddings
-results = [(key, cosine_similarity(compare_embedding.unsqueeze(0), torch.tensor(value).unsqueeze(0)).item()) 
-           for key, value in embeddings.items()]
+results = [(idx, cosine_similarity(compare_embedding.unsqueeze(0), torch.tensor(value).unsqueeze(0)).item()) 
+           for idx, value in embeddings.items()]
 
 # Sort the results by similarity in descending order
 results.sort(key=lambda x: x[1], reverse=True)
@@ -29,21 +37,20 @@ results.sort(key=lambda x: x[1], reverse=True)
 # print the top 10 results
 top=10
 print(f"\nTop {top} most similar words to '{compare_word}':")
-for word, similarity in results[:top]:
-    print(f"{word}: {similarity:.4f}")
+for idx, similarity in results[:top]:
+    print(f"{idx_word[idx]}: {similarity:.4f}")
 
 
 results.sort(key=lambda x: x[1], reverse=False)
 print(f"\nTop {top} most DISSIMILAR words to '{compare_word}':")
-for word, similarity in results[:top]:
-    print(f"{word}: {similarity:.4f}")
-
+for idx, similarity in results[:top]:
+    print(f"{idx_word[idx]}: {similarity:.4f}")
 
 # Print the results
 #for word, dp in results:
 #    print(f"{compare_word} - {word} {dp}")
 
-
+'''
 def find_analogy(a, b, c, embedding_dict, top_n=1):
     """
     Solve analogy: a is to b as c is to ?
@@ -79,3 +86,4 @@ for a, b, c in examples:
         print(f"{a} : {b} :: {c} : {result[0][0]} (score: {result[0][1]:.4f})")
     else:
         print(f"{a} : {b} :: {c} : ? (missing words in vocab)")
+'''
