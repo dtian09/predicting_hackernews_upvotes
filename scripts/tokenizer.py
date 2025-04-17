1b3ee9623729886c8bcb655f9895f4071f4b08fd
evelyntan <evelyntants@gmail.com>
2025-04-17 13:25:54 +0000

    fixed tokenizer

--- scripts/tokenizer.py
+++ scripts/tokenizer.py
@@ -11,49 +11,38 @@
 from collections import Counter
 import re
 
-file_path = "../data/wiki_text_data.txt"
-
-with open(file_path, "r") as file:
-    text = file.read()
-
 def tokenizer(text):
-    # split the string into a list of words by whitespace and remove punctuation
-    text_no_punctuation = re.sub(r'[^\w\s]', ' ', text)
-    list_of_words = text_no_punctuation.split()
+    
+    # remove punctuation and non alphabetic characters 
+    remove_punctuation = re.sub(r'[^\w\s]', '', text)
+    lower_case_words = remove_punctuation.lower()
+    words = lower_case_words.split(' ')
 
-    # lower case the words
-    lower_case_words = [word.lower() for word in list_of_words]
+    # print count of words in split_words_by_whitespace
+    print(f"Number of words before filtering: {len(words)}")
 
-    # get the unique words using set list
-    unique_words = set(lower_case_words)
+    # get word counts 
 
-    # this is much faster than the dictionary comprehension with count()
-    all_word_counts = Counter(lower_case_words)
+    top_k = 30000
+    word_counts = Counter(words)
+    top_words = dict(word_counts.most_common(top_k))
+    word_to_id = {word: i for i, word in enumerate(top_words.keys())}
+    id_to_word = {i: word for i, word in enumerate(top_words.keys())}
 
-    # Build the vocabulary
-    # filter to only include words that appear at least N times
-    N = 5  # minimum frequency threshold
-    unique_word_counts = {word: count for word, count in all_word_counts.items() if count >= N}
-    corpus = list(unique_word_counts.keys())
+    # Sum their counts
+    total_count = sum(count for word, count in top_words.items())
 
-    print('Number of unique words included in word count before filtering: ', len(all_word_counts))
-    print('Number of unique words included in word count after filtering: ', len(unique_word_counts))
+    print(f"Total count of top {top_k} words: {total_count}")
+    # Optional: Show what percentage of all words this represents
+    total_words = sum(word_counts.values())
+    percentage = (total_count / total_words) * 100
+    print(f"This represents {percentage:.2f}% of all words in the corpus")
 
+    # filter corpus to only include words in the tok k words
+    corpus = [word for word in words if word in top_words]
+    print("corpus length:", len(corpus))
 
-    # set vocabulary length to the total number of unique words
-    vocab_length = len(unique_word_counts)
-
-    # create a word to id mapping with the length of ids being the vocab length
-    word_to_id = {word: i for i, word in enumerate(unique_word_counts.keys())}
-
-    # create a id to word mapping with the length of ids being the vocab length
-    id_to_word = {i: word for i, word in enumerate(unique_word_counts.keys())}
-
-    return word_to_id, id_to_word, corpus, unique_word_counts
-
-
-
-
+    return word_to_id, id_to_word, corpus
 
 
 
